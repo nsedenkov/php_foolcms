0f94770679d54fddbfac864ec5695384091cf0b6
N.Sedenkov <nsedenkov@yandex.ru>
2016-03-03 15:44:51 +0300

    2016-03-03 small addition in one template

--- templates/page-xml2mif.php
+++ templates/page-xml2mif.php
@@ -1,2 +1,6 @@
 <h2>Программа обработки файлов XML Росреестра</h2>
-<a href="http://sourceforge.net/projects/xml2mif">SourceForge</a>
+<p>Программа позволяет сформировать картографическое представление (mif/mid) выданных Росреестром кадастровых сведений в формате XML.</p>
+<p>Для работы программа требует наличия в системе интерпретатора Python, а также библиотеки tkinter для графического интерфейса.</p>
+<p>Страница программы на <a href="http://sourceforge.net/projects/xml2mif">SourceForge</a></p></br>
+<p>Прямая загрузка:</p>
+<p><a href="https://sourceforge.net/projects/xml2mif/files/latest/download" rel="nofollow"><img alt="Download xml2mif" src="https://a.fsdn.com/con/app/sf-download-button"></a></p></br>
